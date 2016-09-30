fd02afdaded5a272231f31879ef0b5317504812b
Federico Zarfati <fz234@cornell.edu>
2016-09-30 18:51:01 -0400

    3

--- pytree.py
+++ pytree.py
@@ -3,18 +3,23 @@ from os.path import basename, isdir
 from sys import argv
 
 def tree(startpath):
-    
+    print startpath
     for root, subdirs, files in walk(startpath):
         level = root.replace(startpath, '').count(sep)
         indent = '  | ' * (level-1) + '  ├── '
         print("%s%s/" % (indent, basename(root)))
-        subindent = '  | ' * (level) + '  └──  '
-        for f in files:
-            print("%s%s" % (subindent, f))
-            
+        subindent_1 = '  | ' * (level) + '  ├──  '
+        subindent_2 = '  | ' * (level) + '  └──  '
+        for i, j in enumerate(files):
+            if (i==len(files)-1):
+                print("%s%s" % (subindent_2, j))
+            else:
+                print("%s%s" % (subindent_1, j))
+                
 if __name__ == '__main__':
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
         print('.')
         tree(os.getcwd)
-    else:
+    elif len(sys.argv) == 2:
+        print(sys.argv[1])
         tree(sys.argv[1])
