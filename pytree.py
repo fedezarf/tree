a25baa44fbfa12a354a4bad357e716d9763556a3
Federico Zarfati <fz234@cornell.edu>
2016-10-02 15:33:04 -0400

    rec

--- pytree.py
+++ pytree.py
@@ -1,16 +1,23 @@
-#!/usr/bin/env python3
-
 import sys
 import os
 import re
 import subprocess
 
+c_dir = 0
+c_file = 0
 
 def print_tree(path, indent=""):
+
     files = os.listdir(path)
+    
+    global c_dir
+    global c_file
+    
     for i in range(0, len(files)):
         fullpath = path + "/" + files[i]
 
+        if os.path.isfile(fullpath):
+            c_file += 1 
         if i == len(files) - 1:
             print(indent + "└── " + files[i])
         else:
@@ -18,6 +25,7 @@ def print_tree(path, indent=""):
 
 
         if os.path.isdir(fullpath):
+            c_dir += 1 
             if i == len(files) - 1:
                 print_tree(fullpath, indent+ "    ")
             else:
@@ -33,3 +41,5 @@ else:
     cwd = sys.argv[1]
     print(cwd)
     print_tree(cwd)
+    
+print("\n" + str(c_dir) + " directories, " + str(c_file) + " files")
