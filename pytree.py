9c1a419a32fe9e44407e05317b90df4deadc56bf
Federico Zarfati <fz234@cornell.edu>
2016-10-02 16:24:39 -0400

    climate

--- pytree.py
+++ pytree.py
@@ -7,37 +7,45 @@ import subprocess
 c_dir = 0
 c_file = 0
 
+
 def new_sort(input):
     return re.sub('[^a-zA-Z0-9]', '', input).lower()
+
 
 def lsort(path):
     c = [x for x in os.listdir(path) if not x.startswith('.')]
     return sorted(c, key=new_sort)
 
+
 def print_tree(path, indent=""):
 
     files = lsort(path)
-    
+
     global c_dir
     global c_file
 
     for i in range(0, len(files)):
         fullpath = path + "/" + files[i]
 
-        if os.path.isfile(fullpath):
-            c_file += 1 
+        g_update(fullpath)
+
         if i == len(files) - 1:
             print(indent + "└── " + files[i])
         else:
             print(indent + "├── " + files[i])
 
-
         if os.path.isdir(fullpath):
-            c_dir += 1 
+            c_dir += 1
             if i == len(files) - 1:
                 print_tree(fullpath, indent + "    ")
             else:
                 print_tree(fullpath, indent + "│   ")
+
+
+def g_update(f):
+    global c_file
+    if os.path.isfile(f):
+        c_file +=1
 
 if len(sys.argv) == 1:
     cwd = os.getcwd()
