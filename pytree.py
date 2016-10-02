deca5283e8e23d39f7f8bbba1ada721360fdd323
Federico Zarfati <fz234@cornell.edu>
2016-10-02 15:07:34 -0400

    yes

--- pytree.py
+++ pytree.py
@@ -5,22 +5,25 @@ import os
 import re
 import subprocess
 
-def print_tree(path, indent=""):
 
+def print_tree(path, indent=""):
     files = os.listdir(path)
     for i in range(0, len(files)):
         fullpath = path + "/" + files[i]
 
         if i == len(files) - 1:
-            print(indent + '└──' + files[i])
+            print(indent + "└── " + files[i])
         else:
-            print(indent + '├── ' + files[i])
+            print(indent + "├── " + files[i])
+
 
         if os.path.isdir(fullpath):
             if i == len(files) - 1:
-                print_tree(fullpath, indent + '    ')
+                print_tree(fullpath, indent+ "    ")
             else:
-                print_tree(fullpath, indent + '|    ')
+                print_tree(fullpath, indent+ "│   ")
+
+
 
 if len(sys.argv) == 1:
     cwd = os.getcwd()
